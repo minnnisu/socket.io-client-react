63503941d6bf4706792d21915a65d6349ce21271
minnnisu <mine52100@naver.com>
2023-03-29 14:44:28 +0900

    클라이언트 간 이미지 송수신 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,29 +6,57 @@ import "./App.css";
 function App() {
   const [socket, setSocket] = useState();
   const [msgList, setMsgList] = useState([]);
+  const [jsonTypeCipherText, setJsonTypeCipherText] = useState("");
   const [base64, setBase64] = useState("");
-  const [msg, setMsg] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+
+  const secretKey =
+    "xGHQkCIOr46599weIoqfxiyoBCt4pfBomFAnzuDLfTRTKCj0vZqX9SI4aSVnlKXg";
 
   useEffect(() => {
     const socketIo = io("http://localhost:3001", {
       cors: { origin: "*" },
     });
 
-    // receive message from client
+    // receive text message from server
     socketIo.on("receive message", (msg) => {
       console.log(msg);
       setMsgList((prevMsg) => [...prevMsg, msg]);
     });
 
+    // receive encrypted base64 file from server
+    socketIo.on("receciveBase64", (data) => {
+      // decrypt using DES
+      const decrypted = crypto.DES.decrypt(data.base64Data, secretKey);
+      console.log(decrypted.toString(crypto.enc.Utf8));
+      setBase64(decrypted.toString(crypto.enc.Utf8));
+    });
+
     setSocket(socketIo);
+
+    return () => {
+      if (socket) {
+        socket.emit("disconnect");
+      }
+    };
   }, []);
 
-  // send message to client
+  // send text message to server
   function submitMsg(e) {
     e.preventDefault();
     socket.emit("send message", {
-      name: e.target.name.value,
-      msg: e.target.msg.value,
+      name: userName,
+      msg: userMsg,
+    });
+  }
+
+  // send encrypted base64 file to server
+  function sendImageFile(e) {
+    e.preventDefault();
+    socket.emit("sendBase64", {
+      name: userName,
+      base64Data: jsonTypeCipherText,
     });
   }
 
@@ -38,19 +66,9 @@ function App() {
         encodeFileToBase64(image).then((data) => {
           console.log(data);
           // encrypt using DES
-          const encrypted = crypto.DES.encrypt(
-            data,
-            "xGHQkCIOr46599weIoqfxiyoBCt4pfBomFAnzuDLfTRTKCj0vZqX9SI4aSVnlKXg"
-          );
-          console.log(JSON.stringify(encrypted.ciphertext));
-
-          // decrypt using DES
-          const decrypted = crypto.DES.decrypt(
-            encrypted,
-            "xGHQkCIOr46599weIoqfxiyoBCt4pfBomFAnzuDLfTRTKCj0vZqX9SI4aSVnlKXg"
-          );
-          console.log(decrypted.toString(crypto.enc.Utf8));
-          setBase64(decrypted.toString(crypto.enc.Utf8));
+          const encrypted = crypto.DES.encrypt(data, secretKey).toString();
+          console.log(encrypted);
+          setJsonTypeCipherText(encrypted);
         });
       });
     }
@@ -68,12 +86,11 @@ function App() {
   return (
     <div className="App">
       <div className="input-form">
-        <form onSubmit={submitMsg}>
-          <span>이름</span>
-          <input type="text" name="name" /> <span>메시지</span>
-          <input type="text" name="msg" />
-          <button>전송</button>
-        </form>
+        <span>이름</span>
+        <input type="text" onChange={(e) => setUserName(e.target.value)} />{" "}
+        <span>메시지</span>
+        <input type="text" onChange={(e) => setUserMsg(e.target.value)} />
+        <button onClick={submitMsg}>전송</button>
       </div>
       <div className="msg_container">
         <div className="my_message">
@@ -84,8 +101,11 @@ function App() {
           ))}
         </div>
       </div>
-      <input type="file" onChange={imgChangeHandler} />
-      <img src={base64} alt="Blue Circle"></img>
+      <div>
+        <input type="file" onChange={imgChangeHandler} />
+        <button onClick={sendImageFile}>이미지 전송</button>
+      </div>
+      {base64 && <img src={base64} alt="이미지" />}
     </div>
   );
 }
