f1def23f382376e4f02b3c3fd212c67c903f9aae
minnnisu <mine52100@naver.com>
2023-04-06 22:57:18 +0900

    crypto를 통해 blob 암호화 복호화 스트림으로 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-import crypto from "crypto-js";
 import "./App.css";
 import { useRef } from "react";
-import { Buffer } from "buffer";
 import ss from "socket.io-stream";
-import stream from "stream";
+const crypto = require("crypto");
 const streamToBlob = require("stream-to-blob");
 
 function App() {
@@ -36,7 +34,12 @@ function App() {
     });
 
     ss(socketIo).on("receive", async (tempStream, size) => {
-      const blob = await streamToBlob(tempStream);
+      const password = "McQfTjWnZr4u7x!A";
+      const KEY = Buffer.from(password, "utf8");
+      const IV = Buffer.from(password, "utf8");
+      var decipherStream = crypto.createDecipheriv("aes-128-cbc", KEY, IV);
+      const decipher = tempStream.pipe(decipherStream);
+      const blob = await streamToBlob(decipher);
       const url = window.URL.createObjectURL(blob);
       console.log(url);
       setImg(url);
@@ -63,68 +66,15 @@ function App() {
       var file = event.target.files[0];
       var stream = ss.createStream();
 
+      const password = "McQfTjWnZr4u7x!A";
+      const KEY = Buffer.from(password, "utf8");
+      const IV = Buffer.from(password, "utf8");
+      var encrypt = crypto.createCipheriv("aes-128-cbc", KEY, IV);
       // upload a file to the server.
       ss(socket).emit("file", stream, { size: file.size });
-      ss.createBlobReadStream(file).pipe(stream);
-
-      // Array.from(event.target.files).forEach((image) => {
-      //   encodeFileToBase64(image).then((data) => {
-      //     const base64Array = splitBase64ToByte(data, 200 * 1024);
-      //     socket.emit(
-      //       "send",
-      //       JSON.stringify({ data: encrypteDES(base64Array) })
-      // });
+      ss.createBlobReadStream(file).pipe(encrypt).pipe(stream);
     }
   }
-
-  function encrypteDES(data) {
-    const temp = [];
-    for (const item of data) {
-      temp.push(crypto.DES.encrypt(item, secretKey).toString());
-    }
-    return temp;
-  }
-
-  function decryptDES(data) {
-    let temp = "";
-    for (const item of data) {
-      temp += crypto.DES.decrypt(item, secretKey).toString(crypto.enc.Utf8);
-    }
-    return temp;
-  }
-
-  function splitBase64ToByte(data, length) {
-    let idx = 0;
-    let left = data.length;
-    const base64Array = [];
-    if (data.length < length) {
-      base64Array.push(data.slice(0, Math.floor(data.length / 2)));
-      base64Array.push(data.slice(Math.floor(data.length / 2) + 1));
-    } else {
-      while (true) {
-        if (left > length) {
-          const temp = data.slice(idx, idx + length);
-          base64Array.push(temp);
-          idx += length;
-          left -= length;
-        } else {
-          const temp = data.slice(idx);
-          base64Array.push(temp);
-          break;
-        }
-      }
-    }
-    return base64Array;
-  }
-
-  const encodeFileToBase64 = (image) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(image);
-      reader.onload = (event) => resolve(event.target.result);
-      reader.onerror = (error) => reject(error);
-    });
-  };
 
   return (
     <div className="App">
